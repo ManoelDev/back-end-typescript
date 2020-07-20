51451ae75eaf10ecb31de4dce0154cac2c6f2af8
Manoe Andrade <maguserv@gmail.com>
2020-07-20 03:06:28 -0300

    add import reflec-metadata

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
 import './database';
@@ -5,8 +6,8 @@ import './database';
 const Port = 3333;
 
 const app = express();
-
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(Port, () => {
