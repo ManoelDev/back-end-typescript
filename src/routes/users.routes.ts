5d6d19081dfdece798da73c7e1c8d4202916be2b
Manoe Andrade <maguserv@gmail.com>
2020-07-20 20:46:25 -0300

    Add get users findbyname

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 import User from '../models/Users';
+import UserRespository from '../repositories/UsersRepository';
 
 const usersRouter = Router();
 
 usersRouter.get('/', async (request, response) => {
   const userRepository = getRepository(User);
   const users = await userRepository.find();
+  return response.status(200).json(users);
+});
+
+usersRouter.get('/:name', async (resquest, response) => {
+  const userRepository = getCustomRepository(UserRespository);
+  const users = userRepository.findbyName(resquest.params.name);
   return response.status(200).json(users);
 });
 
